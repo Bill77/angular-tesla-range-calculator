04bfb42ad1d96dca8ce8e399ade1d02b8e13aa24
Chen, Bill <bill.chen@elekta.com>
2016-12-13 11:08:23 -0800

    Working Intial Page

---
+++ src/app/tesla-battery/tesla-battery.module.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TeslaBatteryComponent } from './containers/tesla-battery/tesla-battery.component';
+
+// services
+import { BatteryService } from './tesla-battery.service';
+
+@NgModule({
+    declarations: [
+        TeslaBatteryComponent
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule
+    ],
+    providers: [
+        BatteryService
+    ],
+    exports: [
+        TeslaBatteryComponent
+    ]
+})
+export class TeslaBatteryModule {
+
+}
