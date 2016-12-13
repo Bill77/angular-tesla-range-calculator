d924523a834de784d5ed98217ab9b822dd489c62
Chen, Bill <bill.chen@elekta.com>
2016-12-13 11:25:52 -0800

    Working Car

--- src/app/tesla-battery/tesla-battery.module.ts
+++ src/app/tesla-battery/tesla-battery.module.ts
@@ -2,13 +2,15 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TeslaBatteryComponent } from './containers/tesla-battery/tesla-battery.component';
+import { TeslaCarComponent } from './components/tesla-car/tesla-car.component'; 
 
 // services
 import { BatteryService } from './tesla-battery.service';
 
 @NgModule({
     declarations: [
-        TeslaBatteryComponent
+        TeslaBatteryComponent,
+        TeslaCarComponent
     ],
     imports: [
         CommonModule,
